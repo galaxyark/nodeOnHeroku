18128b9e33e7637940d9a4cd7ab2508b68717fd9
Fangzhou Lu <lfz.ark@gmail.com>
2014-04-22 21:11:01 -0700

    modify

--- helloNode.js
+++ helloNode.js
@@ -1,7 +1,8 @@
 var http = require('http');
+var port = process.env.PORT || 8000;
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello Node.js\n');
-}).listen(5000);
+}).listen(port);
 
-console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at http://127.0.0.1:8000/');
