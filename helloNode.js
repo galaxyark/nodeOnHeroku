0fa5605e3ff049cf776e3e6be5ef483809ba02e5
Fangzhou Lu <lfz.ark@gmail.com>
2014-04-22 21:54:07 -0700

    Test linking with github.

--- helloNode.js
+++ helloNode.js
@@ -5,4 +5,4 @@ http.createServer(function(req, res){
 	res.end('Hello Node.js\n');
 }).listen(port);
 
-console.log('Server running at http://127.0.0.1:8000/');
+console.log('Server running at Heroku');
