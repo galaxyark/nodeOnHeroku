5dcdf28be94d7a350957aec812f49d6e5e916e75
Fangzhou Lu <lfz.ark@gmail.com>
2014-04-29 20:33:25 -0700

    test

--- helloNode.js
+++ helloNode.js
@@ -3,8 +3,8 @@ var url = require('url');
 var port = process.env.PORT || 8000;
 
 http.createServer(function(req, res){
-	var path = url.parse(request.url).pathname;
-	console.log("a request was received for:" + path);
+	// var path = url.parse(request.url).pathname;
+	// console.log("a request was received for:" + path);
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello Node.js\n');
 }).listen(port);
