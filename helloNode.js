d4949f77e19c5120c56adccc5fdc44ed0e183096
Fangzhou Lu <lfz.ark@gmail.com>
2014-04-29 22:30:30 -0700

    support jsonp

--- helloNode.js
+++ helloNode.js
@@ -1,10 +1,17 @@
 var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
 var port = process.env.PORT || 8000;
 
 http.createServer(function(req, res){
-	// var path = url.parse(request.url).pathname;
-	// console.log("a request was received for:" + path);
+	var path = url.parse(req.url).pathname;
+	var urlString = {
+		pathname: url.parse(req.url).pathname,
+		queryparam: querystring.parse(url.parse(req.url).query)
+	};
+
+	callback = urlString.queryparam.callback;
+	console.log("a request was received for:" + callback);
 	res.writeHead(200, {'Content-Type': 'application/json'});
-	res.end("{'hello':'world'}");
+	res.end(callback + "(" + "{'data': 'test'}" + ")");
 }).listen(port);
